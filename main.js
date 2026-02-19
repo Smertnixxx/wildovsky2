baca8426645d096d6f1afd70a0b803d9b4dae036
Smertnixxx <zxcursedwasd@gmail.com>
2026-02-19 22:59:50 +0500

    импорт в мейн

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // 🧹 Fix for ENOSPC / temp overflow in hosted panels
 const fs = require('fs');
 const path = require('path');
-
+const { generateWAMessageFromContent, proto } = require('@whiskeysockets/baileys');
 // Redirect temp storage away from system /tmp
 const customTemp = path.join(process.cwd(), 'temp');
 if (!fs.existsSync(customTemp)) fs.mkdirSync(customTemp, { recursive: true });
